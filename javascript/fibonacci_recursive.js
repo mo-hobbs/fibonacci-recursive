5527a4ad4e7ca63303062dfa1a8b0b4f8eab9b0b
Morgan Byrne <79115095+mo-hobbs@users.noreply.github.com>
2022-03-31 14:22:54 -0400

    tests not passing

--- javascript/fibonacci_recursive.js
+++ javascript/fibonacci_recursive.js
@@ -1,5 +1,18 @@
-function fibonacci(n) {
-  // type your code here
+//n is the nth element in the fib array
+
+function fibonacci(num) {
+
+  var num1=0;
+  var num2=1;
+  var sum;
+  var i=0;
+  for (i = 0; i < num; i++) 
+  {
+      sum=num1+num2;
+      num1=num2;
+      num2=sum;
+  }
+  return num2;
 }
 
 if (require.main === module) {
